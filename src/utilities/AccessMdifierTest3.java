f43f21b1bf819b819aa193ef8d9a166acb6d5d96
hajiraImtiaz <hajiradani315@gmail.com>
2022-04-25 11:35:24 +0200

    day40_accessModifier_TEST3

---
+++ src/utilities/AccessMdifierTest3.java
@@ -0,0 +1,20 @@
+package utilities;
+
+import day40_finalKeyword.protectedAccessModifier;
+
+public class AccessMdifierTest3 {
+
+    public static void main(String[] args) {
+        /*
+        System.out.println(protectedAccessModifier.name);// default is not vissible outside the package
+        System.out.println(protectedAccessModifier.name1);// protected is not visible outside the package untile it is subclass
+
+
+
+         */
+
+
+
+
+    }
+}
