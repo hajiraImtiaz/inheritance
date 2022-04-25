f9b055c5d6655978f3df6c25c2c0852bccbf6a4c
hajiraImtiaz <hajiradani315@gmail.com>
2022-04-25 11:34:46 +0200

    day40_accessModifier_TEST4

--- src/utilities/accessModifier_TEST4.java
+++ src/utilities/accessModifier_TEST4.java
@@ -6,9 +6,9 @@ public class accessModifier_TEST4 extends protectedAccessModifier  {
 
     public static void main(String[] args) {
 
-        System.out.println(protectedAccessModifier.name);// not vissible b/c default is vissible only inside same class and package
+       // System.out.println(protectedAccessModifier.name);// not vissible b/c default is vissible only inside same class and package
         System.out.println(protectedAccessModifier.name1);// vissible b/c protected is vissible in same class, package and any other package subclass
-        accessModifier_TEST4.method1();// default method is not vissible outside pkag
+       // accessModifier_TEST4.method1();// default method is not vissible outside pkag
         accessModifier_TEST4.method2();// protected is visible outside pkg
 
 
